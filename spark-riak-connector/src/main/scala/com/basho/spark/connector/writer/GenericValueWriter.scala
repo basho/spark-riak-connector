997ad1f1966987bd2eec94f60f7c4fe7deb38e4f
Sergey Galkin <srggal@gmail.com>
2015-05-12 17:27:56 +0300

    Adds 2 additional test case to verify/demonstrate how 2i indexes might be created on data save from Spark. It fixes DP-69

--- spark-riak-connector/src/main/scala/com/basho/spark/connector/writer/GenericValueWriter.scala
+++ spark-riak-connector/src/main/scala/com/basho/spark/connector/writer/GenericValueWriter.scala
@@ -1,12 +1,11 @@
 package com.basho.spark.connector.writer
 
 import com.basho.riak.client.core.query.RiakObject
-import com.basho.riak.client.core.util.BinaryValue
 import com.basho.spark.connector.rdd.BucketDef
 import com.basho.spark.connector.util.RiakObjectConversionUtil
 
 class GenericValueWriter[T] (bucketDef: BucketDef) extends ValueWriter[T]{
-  override def mapValue(value: T): (BinaryValue, RiakObject) = {
+  override def mapValue(value: T): (String, Any) = {
     val obj = RiakObjectConversionUtil.to(value)
     (null, obj)
   }
