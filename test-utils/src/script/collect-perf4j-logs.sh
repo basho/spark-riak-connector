8c9f24ecb6a30c96a3beaafa0e939e50b572be72
Anton Nekhaev <anekhaev@dataart.com>
2015-09-18 17:45:31 +0300

    Grabbing CodaHale metrics from workers

--- test-utils/src/script/collect-perf4j-logs.sh
+++ test-utils/src/script/collect-perf4j-logs.sh
@@ -8,15 +8,17 @@ set -e
 )
 
 [ -z "$SSH_USER_NAME" ] && SSH_USER_NAME="ubuntu"
-[ -z "$SSH_KEY_PATH" ] && SSH_KEY_PATH="provide-a-path-to-your.key"
+[ -z "$SSH_KEY_PATH" ] && SSH_KEY_PATH="/home/ubuntu/.ssh/bdp.pem"
 [ -z "$PERF4J_LOG_SRC" ] && PERF4J_LOG_SRC="/home/ubuntu/perf-stats.log"
 [ -z "$PERF4J_LOG_DST" ] && PERF4J_LOG_DST="/home/ubuntu/perf4j-collected-logs"
-
-
-echo "Collecting performance logs from workers: ${WORKER_IPS[@]}..."
+[ -z "$CODAHALE_LOG_SRC" ] && CODAHALE_LOG_SRC="/home/ubuntu/codahale-metrics"
+[ -z "$CODAHALE_LOG_DST" ] && CODAHALE_LOG_DST="/home/ubuntu/codahale-collected-logs"
 
 PERF4J_JAR="perf4j-0.9.16.jar"
 COLLECT_TIME=$(date +"%Y-%m-%d_%T")
+
+echo "Collecting performance logs from workers: ${WORKER_IPS[@]}..."
+
 for WORKER_IP in "${WORKER_IPS[@]}"
 do
 	mkdir -p "$PERF4J_LOG_DST/$COLLECT_TIME"
@@ -31,3 +33,19 @@ done
 
 java -jar "$PERF4J_JAR" "$PERF4J_LOG_DST/$COLLECT_TIME/all-workers-perf.log"  -t 900000000 > "$PERF4J_LOG_DST/$COLLECT_TIME/all-workers-perf.log.stat"
 java -jar "$PERF4J_JAR" --graph "$PERF4J_LOG_DST/$COLLECT_TIME/all-workers-perf.log.html" "$PERF4J_LOG_DST/$COLLECT_TIME/all-workers-perf.log"
+
+
+for WORKER_IP in "${WORKER_IPS[@]}"
+do
+	TARGET_DIR="$CODAHALE_LOG_DST/$COLLECT_TIME"
+        mkdir -p "$TARGET_DIR"
+	SOURCE_FILES=($(ssh -i "$SSH_KEY_PATH" "$SSH_USER_NAME@$WORKER_IP" "ls $CODAHALE_LOG_SRC | grep data-chunk | grep app-"))
+	for SOURCE_FILE in "${SOURCE_FILES[@]}"
+	do
+		echo "Copying performance log from $WORKER_IP: $SOURCE_FILE..."
+		scp -i "$SSH_KEY_PATH" "$SSH_USER_NAME@$WORKER_IP:/$CODAHALE_LOG_SRC/$SOURCE_FILE" "$TARGET_DIR"
+	done
+        echo "Performance log from $WORKER_IP saved to $TARGET_DIR"
+#        cat "$TARGET_FILE" >> "$PERF4J_LOG_DST/$COLLECT_TIME/all-workers-perf.log"
+done
+
