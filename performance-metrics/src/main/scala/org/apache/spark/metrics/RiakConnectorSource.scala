ac2e558a9810c9f538f3f9b5d39055ab79c69f86
Anton Nekhaev <anekhaev@dataart.com>
2015-09-25 20:06:02 +0300

    Timers switched to UniformReservoir

--- performance-metrics/src/main/scala/org/apache/spark/metrics/RiakConnectorSource.scala
+++ performance-metrics/src/main/scala/org/apache/spark/metrics/RiakConnectorSource.scala
@@ -2,6 +2,8 @@ package org.apache.spark.metrics
 
 import com.codahale.metrics
 import org.apache.spark.metrics.source.Source
+import com.codahale.metrics.Timer
+import com.codahale.metrics.UniformReservoir
 
 
 
@@ -14,29 +16,35 @@ class RiakConnectorSource extends Source {
 
   override val metricRegistry = new metrics.MetricRegistry
 
-  val fbr1Full = metricRegistry.timer("fbr-one-query.full")
+  val fbr1Full = timer("fbr-one-query.full")
 
-  val fbr1NotFull = metricRegistry.timer("fbr-one-query.notFull")
+  val fbr1NotFull = timer("fbr-one-query.notFull")
 
-  val fbr2LocationsFull = metricRegistry.timer("fbr-two-queries.locations.full")
+  val fbr2LocationsFull = timer("fbr-two-queries.locations.full")
 
-  val fbr2ValuesFull = metricRegistry.timer("fbr-two-queries.values.full")
+  val fbr2ValuesFull = timer("fbr-two-queries.values.full")
 
-  val fbr2LocationsNotFull = metricRegistry.timer("fbr-two-queries.locations.notFull")
+  val fbr2LocationsNotFull = timer("fbr-two-queries.locations.notFull")
 
-  val fbr2ValuesNotFull = metricRegistry.timer("fbr-two-queries.values.notFull")
+  val fbr2ValuesNotFull = timer("fbr-two-queries.values.notFull")
 
-  val fbr2Full = metricRegistry.timer("fbr-two-queries.full")
+  val fbr2Full = timer("fbr-two-queries.full")
 
-  val fbr2NotFull = metricRegistry.timer("fbr-two-queries.notFull")
+  val fbr2NotFull = timer("fbr-two-queries.notFull")
 
   val fbr2EmptyChunk = metricRegistry.meter("fbr-two-queries.empty")
 
   val fbr2ErrorChunk = metricRegistry.meter("fbr-two-queries.error")
 
   RiakConnectorSource.registerInstance(this)
+
   
+  def timer(name: String) = {
+    metricRegistry.register(name, new Timer(new UniformReservoir()))
+  }
 }
+
+
 
 
 object RiakConnectorSource {
