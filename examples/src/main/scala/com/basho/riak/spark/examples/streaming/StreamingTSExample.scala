251f15677a98473cd737338cbb9d9f1864fd5520
Alex Pavlenko <my.aleksandr.pavlenko@gmail.com>
2016-08-12 20:41:07 +0300

    Streaming example updated to use custom Spark Streaming context

--- examples/src/main/scala/com/basho/riak/spark/examples/streaming/StreamingTSExample.scala
+++ examples/src/main/scala/com/basho/riak/spark/examples/streaming/StreamingTSExample.scala
@@ -3,12 +3,16 @@ package com.basho.riak.spark.examples.streaming
 import java.util.UUID
 
 import kafka.serializer.StringDecoder
-import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.Row
 import org.apache.spark.streaming.Durations
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.sql.types._
+import org.apache.spark.{SparkConf, SparkContext}
+import com.basho.riak.spark.streaming._
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 /**
   * Example shows how Spark Streaming can be used with Riak TS Dataframes
@@ -32,16 +36,7 @@ import org.apache.spark.sql.types._
   *  )
   **/
 object StreamingTSExample {
-
   def main(args: Array[String]): Unit = {
-
-    val schema = StructType(List(
-      StructField(name = "weather", dataType = StringType),
-      StructField(name = "family", dataType = StringType),
-      StructField(name = "time", dataType = TimestampType),
-      StructField(name = "temperature", dataType = DoubleType),
-      StructField(name = "humidity", dataType = DoubleType),
-      StructField(name = "pressure", dataType = DoubleType)))
 
     val sparkConf = new SparkConf(true)
       .setAppName("Simple Spark Streaming to Riak TS Demo")
@@ -52,26 +47,25 @@ object StreamingTSExample {
 
     val sc = new SparkContext(sparkConf)
     val streamCtx = new StreamingContext(sc, Durations.seconds(15))
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    import sqlContext.implicits._
 
     val kafkaProps = Map[String, String](
       "metadata.broker.list" -> sparkConf.get("kafka.broker"),
       "client.id" -> UUID.randomUUID().toString
     )
-    KafkaUtils
-      .createDirectStream[String, String, StringDecoder, StringDecoder](streamCtx, kafkaProps,
-      Set[String]("streaming"))
-      .foreachRDD { rdd => rdd.map(println)
-        val rows = sqlContext.read.schema(schema).json(rdd.values)
-          .withColumn("time", 'time.cast("Timestamp"))
-          .select("weather", "family", "time", "temperature", "humidity", "pressure")
 
-        rows.write
-          .format("org.apache.spark.sql.riak")
-          .mode(SaveMode.Append)
-          .save("ts_weather_demo")
-      }
+    KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](streamCtx, kafkaProps, Set[String]("ingest-ts")
+    ) map { case (key, value) =>
+      val mapper = new ObjectMapper()
+      mapper.registerModule(DefaultScalaModule)
+      val wr = mapper.readValue(value, classOf[Map[String,String]])
+      Row(
+        wr("weather"),
+        wr("family"),
+        DateTime.parse(wr("time"),DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS")).getMillis,
+        wr("temperature"),
+        wr("humidity"),
+        wr("pressure"))
+    } saveToRiakTS "ts_weather_demo"
 
     streamCtx.start()
     streamCtx.awaitTermination()
