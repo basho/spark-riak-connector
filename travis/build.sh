9875f876a25b9b4a52496ad0f5c8e3c808429df8
Jon Brisbin <jon@jbrisbin.com>
2016-08-22 13:41:13 -0500

    Changed secrets to basho org

--- travis/build.sh
+++ travis/build.sh
@@ -13,6 +13,6 @@ else
 fi
 
 if [ "develop" == "$TRAVIS_BRANCH" -o "master" == "$TRAVIS_BRANCH" ] && [ "false" == "$TRAVIS_PULL_REQUEST" ]; then
-  openssl aes-256-cbc -K $encrypted_e4898ca93742_key -iv $encrypted_e4898ca93742_iv -in secrets.tgz.enc -out secrets.tgz -d && tar -zxf secrets.tgz
+  openssl aes-256-cbc -K $encrypted_d60a16d52fe8_key -iv $encrypted_d60a16d52fe8_iv -in secrets.tgz.enc -out secrets.tgz -d && tar -zxf secrets.tgz
   $SBT_CMD sparkRiakConnector/publishSigned
 fi
